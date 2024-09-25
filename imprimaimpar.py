bbb3f484a384568c83bdbde6152e6da177362a5e
aaURIhttp <144628381+aaURIhttp@users.noreply.github.com>
2024-09-24 21:35:56 -0300

    Add files via upload

---
+++ imprimaimpar.py
@@ -0,0 +1,13 @@
+n = int(input("insira um numero:"))
+i = 1
+
+if n <= 0 :
+    print("valor inválido")
+
+
+while i <= n:
+    print(i)
+    i = i + 2
+
+#o usuário insere um valor
+#e o programa imprime os impares até o valor que o usuário informou
