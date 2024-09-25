c2239b0a418e382106d12707f8c19c5337e0dfdc
aaURIhttp <144628381+aaURIhttp@users.noreply.github.com>
2024-09-24 21:18:33 -0300

    Add files via upload

---
+++ contador de ovelha.py
@@ -0,0 +1,11 @@
+contador = 0
+while int(input("Detectou ovelha")) == 1 :
+    contador = contador + 1
+print(f"valor de ovelhas é : {contador}")
+
+
+# Contador de ovelhas
+# O usuário insere 1 para cada ovelha detectada
+# O contador é incrementado a cada detecção
+# O loop para quando um valor diferente de 1 é inserido
+# O total de ovelhas detectadas é exibido no final
